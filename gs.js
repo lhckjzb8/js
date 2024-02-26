ea85d0a7a5c28981176af4e9514e9a60230af87d
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2024-02-26 21:34:27 +0800

    Create gs.js

---
+++ gs.js
@@ -0,0 +1,6 @@
+var k ="yes1,yes,yes1";
+var ktop = "850";//+150
+var zu = "?";
+var zd = "8.jpg";
+var imgurl="www.mm16,1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg";
+var imgurl2="lx36.mh40,tp/guilv1.jpg,tp/guilv2.jpg,tp/guilv3.jpg,tp/guilv4.jpg,tp/guilv5.jpg,tp/guilv6.jpg,tp/guilv7.jpg,tp/guilv8.jpg,tp/guilv9.jpg,tp/guilv10.jpg,tp/guilv11.jpg,tp/guilv12.jpg,tp/guilv13.jpg,tp/guilv14.jpg,tp/guilv15.jpg,tp/guilv16.jpg,tp/guilv17.jpg,tp/guilv18.jpg";
